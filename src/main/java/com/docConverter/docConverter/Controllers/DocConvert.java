e92bb0f639fdd1e2ab599887675491d851c148e3
anupamtechsimians <anupams@techsimians.com>
2023-12-06 14:57:06 +0530

    updated dependency and fixed code

--- src/main/java/com/docConverter/docConverter/Controllers/DocConvert.java
+++ src/main/java/com/docConverter/docConverter/Controllers/DocConvert.java
@@ -28,8 +28,7 @@ public class DocConvert {
     @GetMapping("/hello")
     public String sayHello() {
         try {
-            MultipartFile file = convertFromCDN("https://sample117sample1.s3.ap-south-1.amazonaws.com/sample117sample1/ed9275cc-6f2c-4d40-8371-f0c29d2f514a?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAXUQCJIP5XFFDPSPT%2F20231204%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20231204T085420Z&X-Amz-Expires=600&X-Amz-Signature=988fc45018b292efd61b13c828cb0a90d0514b8fbbdb88ecbceecf93c6914213&X-Amz-SignedHeaders=host");
-            System.out.println(file);
+
             return "Hello, World!";
         }catch (Exception e){
             e.printStackTrace();
@@ -38,18 +37,6 @@ public class DocConvert {
 
     }
 
-//    @CrossOrigin(origins = "http://localhost:4200") // Replace with your frontend origin
-//    @GetMapping("/readFile")
-//    public String readFile() throws IOException {
-//        ClassPathResource resource = new ClassPathResource("sample.json");
-//        InputStream inputStream = resource.getInputStream();
-//
-//        // Read the content of the file into a String
-//        byte[] fileBytes = FileCopyUtils.copyToByteArray(inputStream);
-//        String fileContent = new String(fileBytes, StandardCharsets.UTF_8);
-//
-//        return fileContent;
-//    }
 
     @CrossOrigin(origins = "http://localhost:3000") // Replace with your frontend origin
     @PostMapping("/readFile")
@@ -62,9 +49,6 @@ public class DocConvert {
             return WordProcessorHelper.load(inputStream, FormatType.Docx);
         }catch(Exception e){
             e.printStackTrace();
-//            if(e.getMessage()=="not Found"){
-//                return "file session expired";
-//            }
            return  "{\"sections\":[{\"blocks\":[{\"inlines\":[{\"text\":" + e.getMessage() + "}]}]}]}";
         }
 
